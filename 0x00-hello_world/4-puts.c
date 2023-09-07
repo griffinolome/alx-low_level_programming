6cf9c719f5bf30ce77285090a4b960b2b579979c
griffinolome <griffinolome@gmail.com>
2023-09-07 04:29:32 -0700

    4 puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdioh>
+/**
+ * main - This is found in the beginning of any c code
+ * Return: always found at the end
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle)
+	return (0);
+}
