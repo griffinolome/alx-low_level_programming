19c6f27d6d80538328927dd8cd22a4d2a95710f2
griffinolome <griffinolome@gmail.com>
2023-09-07 04:47:57 -0700

    5 printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - found at the beginning
+ * Return: found at the end
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
