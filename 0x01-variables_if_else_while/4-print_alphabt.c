4a5fbf11cb6480b8c5b63e4a2322c68833dc956f
griffinolome <griffinolome@gmail.com>
2023-09-08 13:15:22 -0700

    4 print alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - beginning of code
+ * Return: end of code
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		if (i != 'q' && i != 'e')
+			putchar(i);
+	putchar('\n');
+	return (0);
+}
