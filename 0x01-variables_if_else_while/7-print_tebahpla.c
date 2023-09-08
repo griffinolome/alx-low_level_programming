9458a2fb0620b2948f36b2f63485e150b2e49216
griffinolome <griffinolome@gmail.com>
2023-09-08 13:38:09 -0700

    7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - in beginning of code
+ * Return: in end of code
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'z' ; i >= 'a' ; i--)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
