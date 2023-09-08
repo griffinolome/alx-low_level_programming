07a7760902841915daf1d8fa56fbb63f5a8b89b4
griffinolome <griffinolome@gmail.com>
2023-09-08 13:47:40 -0700

    8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - beginning of code
+ * Return: end of code
+ */
+int main(void)
+{
+	int i;
+	char j;
+
+	for (i = 0 ; i < 10 ; i++)
+		putchar(i + '0');
+	for (j = 'a' ; j <= 'f' ; i++)
+		putchar(j);
+	putchar('\n');
+	return (0);
+}
