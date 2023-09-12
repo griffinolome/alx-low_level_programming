ea19bd61210d85def802ace6b61290c7d0c85012
griffinolome <griffinolome@gmail.com>
2023-09-12 03:49:24 -0700

    0 correct

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,20 @@
-#include <stdio.h>
+#include "main.h"
 /**
- * main - beginning of code
- * Return: end of your code
+ * main - used in beginning of cose
+ * Return: used at end of code
  */
 
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
-
