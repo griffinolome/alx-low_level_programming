1d9f7bdca529ef6bcd994814a933a7153c41c539
griffinolome <griffinolome@gmail.com>
2023-09-12 09:26:57 -0700

    10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * add - function that sums two numbers
+ * @a: parameter 1
+ * @b: parameter 2
+ * Return: always ends
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
