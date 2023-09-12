59e6db9d2bbb75326c80adb1fbeefc14abe3f789
griffinolome <griffinolome@gmail.com>
2023-09-12 08:31:23 -0700

    9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * times_table - runs code
+ * Return: always 0
+ */
+void times_table(void)
+{
+	int i;
+	int j;
+	int result;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		for (j = 0 ; j < 10 ; j++)
+		{
+			result = i * j;
+			printf("2%d ", result);
+		}
+		printf("\n");
+	}
+}
