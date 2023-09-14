4ed23d139240703b4a1be3d299e60157d8b86a74
griffinolome <griffinolome@gmail.com>
2023-09-14 11:04:16 -0700

    3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - runs the code
+ * Return: end of code
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0 ; i <= 9 ; i++)
+	{
+		_putchar (i + '0');
+	}
+	_putchar ('\n');
+}
