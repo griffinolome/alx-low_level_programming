02b7e0bbad9313112067852c48303dc9fa68bcd6
griffinolome <griffinolome@gmail.com>
2023-09-14 11:34:18 -0700

    5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * more_numbers - runs code
+ * Return: ends code
+ */
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (i = 0 ; i <= 10 ; i++)
+	{
+		for (j = 0 ; j <= 14 ; i++)
+		{
+			_putchar(j + '0');
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
