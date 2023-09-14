7eaeb577d0f4efc4fb566df7a093c8a59d66e7b5
griffinolome <griffinolome@gmail.com>
2023-09-14 13:10:05 -0700

    10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * print_triangle - runs code
+ * @size: size of triangle
+ * Return: end code
+ */
+void print_triangle(int size)
+{
+	int hash;
+	int index;
+
+	if (size > 0)
+	{
+		for (hash = 1 ; hash <= size ; hash++)
+		{
+			for (index = size - hash ; index > 0 ; index--)
+			{
+				_putchar(' ');
+			}
+			for (index = 0 ; index < hash ; index++)
+			{
+				_putchar('#');
+			}
+			if (hash == size)
+			{
+				continue;
+			}
+			_putchar('\n');
+		}
+	}
+}
