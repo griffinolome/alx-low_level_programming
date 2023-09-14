65af75b198f065050e4200a9d9e14917ac64a8ae
griffinolome <griffinolome@gmail.com>
2023-09-14 09:52:01 -0700

    0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isupper - runs the code
+ * @c: parameter in the code
+ * Return: always at end of code
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
