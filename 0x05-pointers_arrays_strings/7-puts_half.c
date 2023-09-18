63813cfd922081cfe7afb58ba77e1aaac6d434b6
griffinolome <griffinolome@gmail.com>
2023-09-18 03:05:39 -0700

    7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * puts_half - runs code
+ * @str: parameter
+ * Return: end of code
+ */
+void puts_half(char *str)
+{
+	int i, n, count = 0;
+
+	for (i = 0 ; str[i] != '\0' ; i++)
+		count++;
+	n = (count - 1) / 2;
+	for (i = n + 1 ; str[i] != '\0' ; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
